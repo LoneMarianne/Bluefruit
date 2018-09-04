3804599b6cbf26a4edb8275b72df2876c13a55e9
LoneMarianne <loneoesterlund@gmail.com>
2018-09-04 13:13:45 +0200

    index org

--- js/index.js
+++ js/index.js
@@ -56,12 +56,10 @@ function refreshDeviceList(){
 
 function onDiscoverDevice(device){
 	//Make a list in html and show devises
-	if(device.name == "LONE"){ //ny if-sætning
 		var listItem = document.createElement('li'),
 		html = device.name+ "," + device.id;
 		listItem.innerHTML = html;
 		document.getElementById("bleDeviceList").appendChild(listItem);
-	}
 }
 
 
